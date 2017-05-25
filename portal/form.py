e97a44c60518d811d9c12829a214e7628a2debf1
Candido Souza <candido_souzza@hotmail.com>
2017-05-25 15:42:07 -0300

    Indentation of code form.py

--- portal/form.py
+++ portal/form.py
@@ -2,8 +2,28 @@ from django import forms
 
 
 class ProductForm(forms.Form):
-    name = forms.CharField(label='Nome', max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    quantity = forms.CharField(label='Quantidade', max_length=10, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    price = forms.CharField(label='Valor', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    short_description = forms.CharField(label='Breve Descrição', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    description = forms.CharField(label='Descrição', widget=forms.Textarea(attrs={'class': 'form-control'}))
+    name = forms.CharField(label='Nome',
+                           max_length=255,
+                           required=True,
+                           widget=forms.TextInput(attrs={'class': 'form-control'})
+                           )
+
+    quantity = forms.CharField(label='Quantidade',
+                               max_length=10,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'})
+                               )
+
+    price = forms.CharField(label='Valor',
+                            required=True,
+                            widget=forms.TextInput(attrs={'class': 'form-control'})
+                            )
+
+    short_description = forms.CharField(label='Breve Descrição',
+                                        required=True,
+                                        widget=forms.TextInput(attrs={'class': 'form-control'})
+                                        )
+
+    description = forms.CharField(label='Descrição',
+                                  widget=forms.Textarea(attrs={'class': 'form-control'})
+                                  )
