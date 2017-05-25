180a39c9fa1be71972fcc3aca3a1023801641eef
Wesley Willians <wesleywillians@gmail.com>
2017-05-25 10:19:44 -0400

    semana3 dev

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^my_ads$', views.my_ads, name='my_ads'),
+    url(r'^product/edit/(?P<product_id>[\d]+)\/?$', views.product_edit, name='product_edit'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, get_object_or_404
+
+from portal.models import Product
 
 
 def home(request):
-    return render(request, 'portal/home.html',{})
+    return render(request, 'portal/home.html', {})
+
+
+def my_ads(request):
+    products = Product.objects.filter(user=request.user)
+
+    context = {
+        'products': products
+    }
+    return render(request, 'portal/my_ads.html', context)
+
+
+def product_edit(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'portal/product_edit.html', context)
