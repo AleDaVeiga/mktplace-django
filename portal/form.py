9273d3a004fb4fdbf98d1a5c6e0f5997ad83ef44
Candido Souza <candido_souzza@hotmail.com>
2017-05-25 15:35:16 -0300

    Fix class input bootstrap

--- portal/form.py
+++ portal/form.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class ProductForm(forms.Form):
-    name = forms.CharField(label='Nome', max_length=255, required=True)
-    quantity = forms.CharField(label='Quantidade', max_length=10, required=True)
-    price = forms.CharField(label='Valor', required=True)
-    short_description = forms.CharField(label='Breve Descrição', required=True)
-    description = forms.CharField(label='Descrição', widget=forms.Textarea)
+    name = forms.CharField(label='Nome', max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    quantity = forms.CharField(label='Quantidade', max_length=10, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    price = forms.CharField(label='Valor', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    short_description = forms.CharField(label='Breve Descrição', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(label='Descrição', widget=forms.Textarea(attrs={'class': 'form-control'}))
