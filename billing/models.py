f14326eed396b7887d464b6693d418da2c1919a2
Candido Souza <candido_souzza@hotmail.com>
2017-06-12 11:34:59 -0300

    Creating model order

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='order_user')
+    merchant = models.ForeignKey(User, related_name='order_merchant')
+    commission = models.DecimalField(max_digits=8, decimal_places=2)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Refused', 'Refused'),
+        ('Approved', 'Approved'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Inactive")
+
+    def __unicode__(self):
+        return "#" + str(self.id)
+
